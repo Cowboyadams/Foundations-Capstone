d0f61c9b6bf9f0ace9d263197bb62414cfcbacf8
Dallin Adams <d.rob.adams@gmail.com>
2022-07-11 16:16:35 -0600

    Added Features list

--- public/index.js
+++ public/index.js
@@ -42,7 +42,57 @@ const localList = [
         ['tundra','tundra-hills','artic-mountain-range','taigia','frozen-desert','polar-ocean',         'wasteland'],
         ['tundra','tundra-hills','artic-mountain-range','taigia','frozen-wastes','polar-ocean',     'frozen-wastes']
     ]
-let ManualChunkList =[]
+const plainsFeatures = [
+    ["City"],
+    ["Small Town", "Ghost Town", "Homestead", "Horse Herd"],
+    ['Road', "Ravine", "Hot Springs", "Cultivated Farmland", "Cattle Herd"],
+    ["Springs", "River", "Trail"],
+    ["Pond", "Stream"]
+]
+const hillsFeatures = [
+    ["Underground City"],
+    ["Small Town","Ghost Town", "Homestead", "Abandoned Mine"],
+    ["Road", "Ravine", "Hot Springs", "Gyser"],
+    ["Springs", "River", "Trail", "Valley", "Lake"],
+    ["Ponds", "Streams", "Waterfall"]
+]
+const mountainFeatures = [
+    ["Active Volcano"],
+    ["Homestead", "Lava Flow", "Open Caves", "Dormant Volcano"],
+    ["Road", "Ravine", "Hot Springs", "Glacier"],
+    ["Springs", "River", "Meadow", "Trail", "Valley", "Rock Glacier", "Lake"],
+    ["Ponds", "Streams", "Gully", "Water Fall"]
+]
+const forestFeatures = [
+    ["Ancient Temple"],
+    ["small Town", "Ghost town", "Homestead", "Ancient Shrine", "Treehome"],
+    ["Road", "Ravine", "Hot Springs", "Bamboo Forest"],
+    ["Springs", "River", "Trail", "Lake"],
+    ["Pond", "Stream"]
+]
+const desertFeatures = [
+    ["Ancient Temple"],
+    ["Oil Resevior", "Desert Road", "Hot Springs", "Bandit Camps"],
+    ["Ravine", "Badlands", "Mesa"],
+    ["Dried River Bed", "Dried Lake Bed"],
+    ["Sand pit"]
+]
+const oceanFeatures = [
+    ["Inhabited Island", "Treasure Island"],
+    ["Large Island", "Small Island", "Shipping Lane", "Floatsam Island", "Archipelago"]
+    ["Hot Plumes", "Underwater Trench", "Shipwreck", "Whirlpool"],
+    ["Coral Reef", "Ocean Floor", "Kelp Forest", "Atoll"],
+    ["Islets"]
+]
+const wastelandFeatures = [
+    ["Oasis"],
+    ["Oil Resivior", "Skeletal Remains", "Toxic Pits"],
+    ["Blightlands", "Lava Fields", "Giant Skeleton"],
+    ["Wasteland", "Radioactive Dust"],
+    ["Wasteland"]
+]
+
+    let ManualChunkList =[]
 //identifiers
 let asdf = chunkList.length - 1
 let lastChunk = chunkList[asdf]
